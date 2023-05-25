f3d185e3ca182277ce91a9abe959bf7d668a117f
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2023-05-24 22:08:48 -0500

    done, maybe?

--- thejs.js
+++ thejs.js
@@ -1,27 +1,82 @@
 /* TODO: on page load, animate all the cards like dominos to show that they can be flipped */
 
 async function populateCards() {
-	// fetch json from root and load into memory
-	const url = 'content.json'
-	const request = new Request(url)
-	const response = await fetch(request)
-	// the json data that populates the tiles
-	const thingsAboutMe = await response.json()
-
-	// get ui elements
+	
+	// get a reference to the main section
 	const main = document.querySelector('main')
-	const tiles = document.querySelectorAll('.tile')
 
-	// TODO: randomly select objects from the json array
+	// to store the json data
+	let thingsAboutMe = null
+
+	// const request = new Request(url)
+	// const response = await fetch(request)
+	// // the json data that populates the tiles
+	// const thingsAboutMe = await response.json()
+	// shuffle(thingsAboutMe)
+	
+	
+	
+	// fetch json from root and load into memory
+	try {
+		const url = 'content.json'
+		const request = new Request(url)
+		try {
+			const response = await fetch(request)
+			try {
+				// the json data that populates the tiles
+				thingsAboutMe = await response.json()
+				shuffle(thingsAboutMe)
+			} catch (responseError) {
+				console.log('There was an error with the response when getting the JSON data.')
+				console.log(responseError)
+			}
+		} catch (fetchError) {
+			console.log('There was an error fetching the requested JSON data.')
+			console.log(fetchError)
+		}
+	} catch (requestError) {
+		console.log('There was an error requesting the JSON data.')
+		console.log(requestError)
+	}
+
+	// guard against JSON failure - return from this function since the rest needs the JSON data
+	if (!thingsAboutMe) return
+
+	// list of tile bg colors to randomly choose from	
+	const bgColors = [
+		'--gradient-purple-pink',
+		'--gradient-blue-green',
+		'--gradient-pink-green',
+		'--gradient-yellow-pink',
+		'--gradient-pink-green2',
+		'--gradient-blue-green', //
+		"--gradient-coral-peachpuff",
+		"--gradient-atomictangerine-salmon",
+		"--gradient-limegreen-dodgerblue",
+		"--gradient-beige-tomato",
+		"--gradient-orange-yellow",
+		"--gradient-skyblue-royalblue",
+		"--gradient-crimson-deeppink",
+		"--gradient-red-orangered",
+		"--gradient-gold-darkorange",
+		"--gradient-deepskyblue-blue",
+		"--gradient-darkorchid-darkviolet",
+		"--gradient-firebrick-sienna",
+		"--gradient-darkorange-coral",
+		"--gradient-steelblue-cornflowerblue",
+		"--gradient-palevioletred-hotpink"
+	];
+	
 	// generate a new tile for each thing about me
 	let animationDelay = 0
 	const ANIMATION_DELAY_INTERVAL = 0.3 // seconds
 	const imgDir = 'img'
+	
+	// build the new nodes, set attributes, and populate them
 	for (let i = 0; i < thingsAboutMe.length; i++) {
 
 		const thisThing = thingsAboutMe[i]
 
-		// build the new nodes, set attributes, and populate them
 		const tile = document.createElement('article')
 		tile.id = thisThing['id']
 		tile.className = 'tile'
@@ -30,7 +85,7 @@ async function populateCards() {
 
 		const cardContainer = document.createElement('div')
 		cardContainer.className = 'card-container'
-		// hide this wrapper from screen readers as it breaks the semantics
+		// hide this from screen readers as it breaks the semantics (I think?)
 		cardContainer.ariaHidden = "true"
 
 		const cardFront = document.createElement('section')
@@ -47,79 +102,62 @@ async function populateCards() {
 		// not using .textContent bc it renders html as plaintext -- need links
 		text.innerHTML = thisThing['text']
 
-		// put the nodes together
+		// put the nodes together for this card
 		main.append(tile)
 		tile.append(cardContainer)
 		cardContainer.append(cardFront, cardBack)
 		cardFront.append(img)
 		cardBack.append(text)
 
+		// TODO: maybe, someday, launch the youtube vids in modals instead of a new tab/window
+
 		// add image citation to credits list
 		const credits = document.querySelector('.credits ul')
 		const listItem = document.createElement('li')
-		listItem.innerHTML = thisThing['img_link']
+		listItem.innerHTML = thisThing['img_link'] + '&nbsp; : : : &nbsp;'
 		credits.append(listItem)
 
-		// style the tile with a random background color
-		const bgColors = [
-			'--gradient-purple-pink',
-			'--gradient-blue-green',
-			'--gradient-pink-green',
-			'--gradient-yellow-pink',
-			'--gradient-pink-green2',
-			'--gradient-blue-green', //
-			"--gradient-coral-peachpuff",
-			"--gradient-atomictangerine-salmon",
-			"--gradient-limegreen-dodgerblue",
-			"--gradient-beige-tomato",
-			"--gradient-orange-yellow",
-			"--gradient-skyblue-royalblue",
-			"--gradient-crimson-deeppink",
-			"--gradient-red-orangered",
-			"--gradient-gold-darkorange",
-			"--gradient-deepskyblue-blue",
-			"--gradient-darkorchid-darkviolet",
-			"--gradient-firebrick-sienna",
-			"--gradient-darkorange-coral",
-			"--gradient-steelblue-cornflowerblue",
-			"--gradient-palevioletred-hotpink"
-		];
 		const randomColor = randomChoiceFromArray(bgColors)
 		const bgColorFront = `var(${randomColor})`
 		const bgColorBack = bgColorFront
 		cardFront.style.backgroundImage = bgColorFront
 		// add a bg image + a translucent gradient atop it
-		// cardBack.style.background =
-		// 	`${bgColorBack}, url(${imgDir}/${thisThing['img']}), hsla(0, 0%, 0%, 0.512)`
 		cardBack.style.background =
 			`linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.1)), ${bgColorBack}, url(${imgDir}/${thisThing['img']})`
-			//  /* top layer */
-      // url("img/frog.png"), /* middle layer */
-      // linear-gradient(to bottom, blue, pink)
 		cardBack.style.backgroundPosition = 'center'
 		cardBack.style.backgroundSize = '100%'
-
-		
-
-		debugger
-
-
 	}
-
-
-
-
-	// works up to here
-	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 }
 
 
 /* Returns a random choice from an array. */
 function randomChoiceFromArray(array) {
-	const rand = Math.floor(Math.random() * (array.length))
+	const rand = Math.floor(Math.random() * array.length)
 	return array[rand]
 }
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
+/* Returns an array of YouTube url strings. Assumes thisThing.text has video links. Stopped short of using this with the goal of creating modals for each youtube link... maybe one day. */
+// function getVideoLinksFrom(thisThingText) {
+// 	// assume thisThing has video links
+// 	// find a youtube URL ending with a single quote (')
+// 	const regex = /https:\/\/www\.youtube\.com\/watch\?[^ ]*'/g;
+// 	const matches = thisThingText.match(regex)
+// 	const urls = []
+// 	for (let i = 0; i < matches.length; i++) {
+// 		// remove the trailing single quote from this url
+// 		let match = matches[i].slice(0, -1)
+// 		urls.push(match)
+// 	}
+// 	return urls
+// }
+
+
+// RUN THE MAIN FUNCTION
 populateCards()
